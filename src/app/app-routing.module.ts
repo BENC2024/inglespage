d633109ec1c7194fbead817bd06ffe511dd46ddd
brahian github <brahiangithub@gmail.com>
2023-10-02 10:36:03 -0500

    ingles page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AnimalsComponent } from './animals/animals.component'
+import { ColorsComponent } from './colors/colors.component';
+import { FruitsComponent } from './fruits/fruits.component';
+import { VowelsComponent } from './vowels/vowels.component';
+import { PresentationComponent } from './presentation/presentation.component';
+
+const routes: Routes = [
+   {path: 'animals', component: AnimalsComponent},
+   {path: 'colors', component: ColorsComponent},
+   {path: 'fruits', component: FruitsComponent},
+   {path: 'vowels', component: VowelsComponent},
+   {path: '', component: PresentationComponent},
+   {path: 'presentation', component: PresentationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AnimalsComponent } from './animals/animals.component';
+import { ColorsComponent } from './colors/colors.component';
+import { FruitsComponent } from './fruits/fruits.component';
+import { VowelsComponent } from './vowels/vowels.component';
+import { PresentationComponent } from './presentation/presentation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnimalsComponent,
+    ColorsComponent,
+    FruitsComponent,
+    VowelsComponent,
+    PresentationComponent
   ],
   imports: [
     BrowserModule,
